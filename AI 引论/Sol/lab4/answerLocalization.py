6295b18fd681f70399b9165b1c87437bbd5f8e65
Kingsley-Yoimiya <153263432+Kingsley-Yoimiya@users.noreply.github.com>
2024-06-06 16:16:08 +0800

    temp q1

--- AI 引论/Sol/lab4/answerLocalization.py
+++ AI 引论/Sol/lab4/answerLocalization.py
@@ -8,6 +8,18 @@ MAX_ERROR = 50000
 
 ### 可以在这里写下一些你需要的变量和函数 ###
 
+def checkStatus(position, walls):
+    for x in walls:
+        if ((x - position) ** 2).sum() <= 0.5: 
+            return False
+    return True
+
+def generNode(walls, rightMost):
+    position = np.random.rand(1, 2) * rightMost
+    # print("FIND:", position)
+    if not checkStatus(position, walls):
+        return generNode(walls, rightMost)
+    return position
 
 def generate_uniform_particles(walls, N):
     """
@@ -18,13 +30,18 @@ def generate_uniform_particles(walls, N):
     particles: List[Particle], 返回在空地上均匀采样出的N个采样点的列表，每个点的权重都是1/N
     """
     all_particles: List[Particle] = []
-    for _ in range(N):
-        all_particles.append(Particle(1.0, 1.0, 1.0, 0.0))
     ### 你的代码 ###
-    
+    rightMost = np.max(walls, axis = 0)
+    # print("RIGHTMOST: ", rightmost)
+    for _ in range(N):
+        position = generNode(walls, rightMost)
+        # print("GET: ", position)
+        all_particles.append(Particle(position[0][0], position[0][1], np.random.uniform(0, 2 * np.pi), 1 / N))
+    # print(all_particles)
     ### 你的代码 ###
     return all_particles
 
+CALCWEIGHT_K = 0.0001
 
 def calculate_particle_weight(estimated, gt):
     """
@@ -34,11 +51,12 @@ def calculate_particle_weight(estimated, gt):
     输出：
     weight, float, 该采样点的权重
     """
-    weight = 1.0
     ### 你的代码 ###
-    
+    # print("!",np.sqrt(((estimated - gt) ** 2).sum()))
+    # print("!!!", np.exp(-CALCWEIGHT_K * np.sqrt(((estimated - gt) ** 2).sum())))
+    return np.exp(-CALCWEIGHT_K * np.sqrt(((estimated - gt) ** 2).sum()))
+    # print(weight)
     ### 你的代码 ###
-    return weight
 
 
 def resample_particles(walls, particles: List[Particle]):
@@ -50,10 +68,47 @@ def resample_particles(walls, particles: List[Particle]):
     particles: List[Particle], 返回重采样后的N个采样点的列表
     """
     resampled_particles: List[Particle] = []
-    for _ in range(len(particles)):
-        resampled_particles.append(Particle(1.0, 1.0, 1.0, 0.0))
     ### 你的代码 ###
-    
+    resWeight, cnt, tot = 1, 0, len(particles)
+    leftMost = np.min(walls, axis = 0)
+    rightMost = np.max(walls, axis = 0)
+    # print("!" , leftMost, rightMost)
+    resWeight = 0
+    for x in particles:
+        resWeight += x.weight
+    # print(resWeight, tot)
+    for x in particles:
+        x.weight *= 1 / resWeight
+    resWeight = 0
+    for x in particles:
+        resWeight += x.weight
+    # print(resWeight, tot)
+
+    resWeight = 0
+
+    for x in particles:
+        # print(x.weight)
+        resWeight += x.weight
+        ncnt = int(np.floor(resWeight * tot))
+        for _ in range(ncnt):
+            y = x
+            y.weight = resWeight / ncnt
+            # print(y.position, np.random.normal(0, 1, 2))
+            y.position += np.random.normal(0, 0.5, 2)
+            y.position[0] = max(leftMost[0], min(y.position[0], rightMost[0]))
+            y.position[1] = max(leftMost[1], min(y.position[1], rightMost[1]))
+            while not checkStatus(y.position, walls):
+                y.position = x.position + np.random.normal(0, 0.5, 2)
+                y.position[0] = max(leftMost[0], min(y.position[0], rightMost[0]))
+                y.position[1] = max(leftMost[1], min(y.position[1], rightMost[1]))
+            y.theta += np.random.normal(0, 0.01)
+            resampled_particles.append(y)
+            resWeight -= y.weight
+        cnt += ncnt
+    # print(resWeight, tot - cnt, cnt)
+    for _ in range(tot - cnt):
+        v = generNode(walls, rightMost)
+        resampled_particles.append(Particle(v[0][0], v[0][1], np.random.uniform(0, 2 * np.pi), resWeight / (tot - cnt)))
     ### 你的代码 ###
     return resampled_particles
 
@@ -65,7 +120,10 @@ def apply_state_transition(p: Particle, traveled_distance, dtheta):
     particle: 按照相同方式进行移动后的粒子
     """
     ### 你的代码 ###
-
+    p.theta += dtheta
+    # print([np.cos(p.theta), np.sin(p.theta)])
+    # print(traveled_distance)
+    p.position += [np.cos(p.theta) * traveled_distance, np.sin(p.theta) * traveled_distance]
     ### 你的代码 ###
     return p
 
@@ -76,8 +134,5 @@ def get_estimate_result(particles: List[Particle]):
     输出：
     final_result: Particle, 最终的猜测结果
     """
-    final_result = Particle()
-    ### 你的代码 ###
-    
-    ### 你的代码 ###
-    return final_result
+    print("RESULT:", max(particles, key = lambda x : x.weight).position)
+    return max(particles, key = lambda x : x.weight)
