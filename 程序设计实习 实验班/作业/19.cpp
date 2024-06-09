f7af49b90f44364bafc4f9ee4a349252d1a8b64f
Kingsley-Yoimiya <153263432+Kingsley-Yoimiya@users.noreply.github.com>
2024-06-09 14:17:25 +0800

    reset to lab4 initial

---
+++ 程序设计实习 实验班/作业/19.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+
+#define rep(i, x, y) for(int i = (x); i <= (y); i++)
+#define per(i, x, y) for(int i = (x); i >= (y); i--)
+
+using namespace std;
+
+template < typename T > void chkmax(T &x, const T &y) { x = x < y ? y : x; }
+template < typename T > void chkmin(T &x, const T &y) { x = x < y ? x : y; }
+
+using db = long double;
+using vec = vector < db >;
+
+const int N = 5e5 + 10;
+const int mod = 1e9 + 7;
+const db eps = 1e-6;
+
+mt19937 rnd(time(NULL));
+normal_distribution<> gen(0, 1);
+
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("1.in", "r", stdin);
+    freopen("1.out", "w", stdout);
+    #endif
+
+    return 0;
+}
